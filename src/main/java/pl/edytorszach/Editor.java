b5b580ba52f4b2ba2cffd5cfeb217cd68fadbe0b
Patyszoon <pati0755@tlen.pl>
2026-05-19 13:59:58 +0200

    switch case for Editor

--- src/main/java/pl/edytorszach/Editor.java
+++ src/main/java/pl/edytorszach/Editor.java
@@ -24,6 +24,14 @@ public class Editor {
 
     public void run(){
         output.println("CHESSBOARD EDITOR. What do you want to do?:");
+        output.println("Please choose one:\n" +
+                "create chessboard\n" +
+                "create obstacles\n" +
+                "move knight\n" +
+                "show attacks\n" +
+                "save to file\n" +
+                "load from file\n" +
+                "exit");
         try{
             String line;
             while ((line = input.readLine()) != null) {
@@ -36,7 +44,32 @@ public class Editor {
 
     private void processCommand(String input){
         // TODO: switch case z komendami
-
-
+        switch(input.toLowerCase()){
+            case "create chessboard":
+                output.println("Creating new chessboard. Please provide size:");
+                break;
+            case "create obstacles":
+                output.println("Creating obstacles. Please provide position:");
+                break;
+            case "move knight":
+                output.println("Moving knight. Please provide new position:");
+                break;
+            case "show attacks":
+                output.println("Attacked fields:");
+                break;
+            case "save to file":
+                output.println("Saving chessboard to file.");
+                fileManager.save();
+                break;
+            case "load from file":
+                output.println("Loading chessboard from file.");
+                fileManager.load();
+                break;
+            case "exit":
+                System.exit(0);
+            default:
+                output.println("Unknown command.");
+                break;
+        }
     }
 }
